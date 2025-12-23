622a5fba4806abef05ec809055e75ab6d4746722
Bishu Thapa <thapabishu435@gmail.com>
2025-12-23 22:53:57 +0545

    database connection

--- core/database/db.ts
+++ core/database/db.ts
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose'
 import { DB_NAME } from '@/constant'
 
+const MongoBDURI = process.env.MONGODB_URI;
+
+if(!MongoBDURI){
+    throw new Error("Please provide the mongoDB URI to the .env file");
+}
+
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
