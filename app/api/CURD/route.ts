572315a7c6706ab9413b40238dc17009ef7ac423
Bishu Thapa <thapabishu435@gmail.com>
2025-12-25 19:02:38 +0545

    implement GET and POST endpoints for blog management

---
+++ app/api/CURD/route.ts
@@ -0,0 +1,64 @@
+import { Blog } from "@/core/models/index.model"
+import {  NextResponse } from "next/server"
+import { IBlog, creatBlog } from "@/types";
+import { ObjectId } from "mongodb";
+import connectDB from "@/core/database/db";
+
+export async function GET(): Promise<NextResponse<IBlog[] | {error: unknown}>> {
+
+    try{
+
+    await connectDB();
+
+    const blogs = await Blog.find({});
+    return NextResponse.json(blogs, {
+        status: 200
+    });
+}
+catch(e){
+    return NextResponse.json({error: e},
+        {
+            status: 500
+        }
+    )
+}
+
+}
+
+export async function POST(request: Request) : Promise<NextResponse<IBlog | {error: string}>> {
+
+    try{
+
+        const body : creatBlog = await request.json();
+
+        if(!body.title && !body.author && !body.content){
+            return NextResponse.json(
+                {error: `Title, content, author are required`},
+                {
+                    status: 400
+                }
+            );
+        }
+
+        await connectDB();
+        const result: IBlog = await Blog.create({
+            _id : new ObjectId(),
+            title: body.title,
+            author: body.author,
+            content: body.content,
+            tags: body.tags,
+            published: true,
+        });
+        return NextResponse.json(result, {
+            status: 201
+        });
+    }
+    catch(e){
+        return NextResponse.json({error: "error", e},
+            {
+                status: 500
+            }
+        )
+
+    }
+}
