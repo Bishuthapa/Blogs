0116c47586227b38fcd68ab22a0c52be88c15eb0
Bishu Thapa <thapabishu435@gmail.com>
2025-12-26 17:54:50 +0545

    feat: implement User model with schema for user management

---
+++ core/models/User.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+import { IUser } from "@/types";
+
+const UserSchema = new Schema<IUser>(
+    {
+        username:{
+            type: String,
+            required: true,
+            maxlength: [25, "Username cannot exceed 25 characters"],
+        },
+
+        email:{
+            type: String,
+            required: [true, "Email is required"],
+        },
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+
+        avatar: {
+            type: String,
+            default : String,
+        }
+    },
+    {
+        timestamps: true
+    },
+)
+
+
+export const User = models.User || model<IUser>("User", UserSchema);
