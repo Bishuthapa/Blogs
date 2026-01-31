f424b3d285d8b3769f45d69c721d9c27423c43e0
Bishuthapa <thapabishu435@gmail.com>
2026-01-31 17:15:18 +0545

    add the forget password and reset password function

--- app/api/auth/users/login/route.ts
+++ app/api/auth/users/login/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { LoginSchema } from '@/validators/loginSchema';
+import {  ForgetPasswordSchema, ResetPasswordSchema } from "@/validators/forgotPasswordSchema"
 import connectDB from "@/core/database/db";
 import { User } from "@/core/models/User.model";
 import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/utils/mailer";
 
 connectDB();
 
@@ -12,6 +14,88 @@ export async function POST(req: NextRequest) {
 
     try {
         const body = await req.json();
+        const { action } = body;
+
+        if(action === "forgot-password"){
+            const result = ForgetPasswordSchema.safeParse(body);
+
+            if(!result.success){
+                return NextResponse.json({
+                    success: false,
+                    error: result.error.flatten().fieldErrors,
+                },
+            {
+                status: 400
+            });
+            }
+
+            const { email } = result.data;
+
+            const user = await User.findOne({ email });
+            if(!user) {
+                return NextResponse.json({
+                    success : true,
+                    message: "If that email exists, a reset link has been sent",
+                });
+            }
+
+            await sendEmail({
+                email: user.email,
+                emailType: "reset",
+                userId: user._id.toString(),
+            });
+
+
+            return NextResponse.json({
+                success: true,
+                message: "Password reset email sent successfully",
+            });
+        }
+
+        if(action === "reset-password"){
+            const result = ResetPasswordSchema.safeParse(body);
+
+            if(!result.success){
+                return NextResponse.json({
+                    success: false,
+                    error: result.error.flatten().fieldErrors,
+                },
+            {
+                status: 400
+            });
+            }
+
+            const {token, password} = result.data;
+
+
+            const user = await User.findOne( {
+                forgetPasswordToken : token,
+                forgetPasswordTokenExpiry: { $gt: Date.now()},
+            });
+
+            if(!user) {
+                return NextResponse.json({
+                    success: false,
+                    message: "Invalid or exprired reset token",
+                },{
+                    status: 400
+                });
+            }
+
+            const hashPassword = await bcryptjs.hash(password, 10);
+
+            user.password = hashPassword;
+            user.forgetPasswordToken = undefined;
+            user.forgetPasswordTokenExpiry = undefined;
+            await user.save();
+
+            return NextResponse.json({
+                success: true,
+                message: "Password reset successfully",
+            });
+        }
+
+
         const result = LoginSchema.safeParse(body);
 
 
