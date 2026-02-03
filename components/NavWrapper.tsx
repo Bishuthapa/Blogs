c13a34512c624790bfd0419181d9e2a08b065f41
Bishuthapa <thapabishu435@gmail.com>
2026-02-03 16:54:02 +0545

    add the nav bar

---
+++ components/NavWrapper.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import NavBar from "./navBar";
+
+const HIDDEN_NAV_PATHS = ["/login", "/signup"];
+
+export default function NavWrapper() {
+  const pathname = usePathname();
+
+  const shouldHideNav = HIDDEN_NAV_PATHS.some((path) =>
+    pathname.startsWith(path)
+  );
+
+  if (shouldHideNav) {
+    return null;
+  }
+
+  return <NavBar />;
+}
+
---
+++ components/navBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return(
+        <> 
+        <div className="text-2xl flex justify-items-center-safe mb-6 bg-gray-600">
+            <a href="/profile" className="px-3" >Home</a>
+            <a className="px-3">About</a>
+        </div>
+        </>
+    )
+}
