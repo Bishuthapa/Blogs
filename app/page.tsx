dcf5b29f340d9335c82477c4f401ec33236ef40a
Bishuthapa <thapabishu435@gmail.com>
2026-02-03 23:48:16 +0545

    fix the error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[var(--background)] text-[var(--foreground)] font-sans">
+    <div className="flex min-h-screen items-center justify-center font-sans">
       Try something and return
     </div>
   );
