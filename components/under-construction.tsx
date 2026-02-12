38432c31b1fb2c6003e2c834cd89356228d678a3
Bishuthapa <thapabishu435@gmail.com>
2026-02-12 21:40:26 +0545

    add the under-development page

---
+++ components/under-construction.tsx
@@ -0,0 +1,11 @@
+
+export default function UnderDevelopment(){
+    return (
+        <>
+        <div>
+            Page is on under construction. Come back later
+
+        </div>
+        </>
+    )
+}
