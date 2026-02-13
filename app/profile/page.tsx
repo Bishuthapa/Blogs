f1419eb31a2e3f030e10c64736f53030962e4058
Bishuthapa <thapabishu435@gmail.com>
2026-02-13 14:12:26 +0545

    update the under-construction page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,16 @@
-export default function Profile(){
-    return(
+import UnderDevelopment from "@/components/under-construction";
+
+
+export default function Profile() {
+    //const isLive = true;
+    const isLive = false;
+
+    return isLive ? <>
+        <p>gojo</p>
         <>
-            <p>gojo</p>
+
         </>
-    )
+    </>
+        :
+        <UnderDevelopment />
 }
