002b43e65df1c468acd86ee72319621fce4094fd
Bishuthapa <thapabishu435@gmail.com>
2026-02-15 15:25:47 +0545

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ import UnderDevelopment from "@/components/under-construction";
 
 export default function Welcome() {
 
-  const isLive = true;
+  const isLive = false;
   
 
-  return isLive  ? <p>Landing Page</p> : <UnderDevelopment />;
+  return isLive  ? 
+  
+  <p>Landing Page</p> 
+  
+  
+  
+  
+
+
+  : <UnderDevelopment />;
 }
 
 
