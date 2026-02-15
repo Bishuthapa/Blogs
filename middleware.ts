60c4c04feab04848dbb970d82b106394b5ab2300
Bishuthapa <thapabishu435@gmail.com>
2026-02-15 16:56:37 +0545

    fix the rotue issue

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.get("token")?.value;
-
-    if (request.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/home', request.url));
-    }
-
 
     // Protected routes that require authentication
     const protectedRoutes = ["/profile"];
@@ -34,8 +30,6 @@ export function middleware(request: NextRequest) {
     }
 
     return NextResponse.next();
-
-    
 }
 
 
