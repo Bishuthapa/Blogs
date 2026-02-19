7515fa16a0e82ce7a9b3d38a11cb8cd8a7f53361
Bishuthapa <thapabishu435@gmail.com>
2026-02-19 22:25:33 +0545

    fix the robots.txt file

--- app/Robots.ts
+++ app/Robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/api/auth/",
     },
     sitemap: "https://bishesh0.com.np/sitemap.xml",
   };
